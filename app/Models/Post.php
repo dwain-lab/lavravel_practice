4f9fd0a286f88a39d5f6e515396873dedfd0b21e
Dwain <dwain22@gmail.com>
2020-12-07 17:59:50 -0600

    Fix CSS issue on Post Forms

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,18 +6,19 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
+use App\Models\User;
 
 class Post extends Model
 {
     use HasFactory, LogsActivity;
 
-    protected static $logName = 'Hello';
+    protected static $logName = 'PostTable';
+
+    protected static $logAttributes = ['title', 'description'];
 
     protected $fillable = [
         'title',
         'description',
     ];
-
-    protected static $logAttributes = ['title', 'description'];
 
 }
--- resources/views/post/post-table.blade.php
+++ resources/views/post/post-table.blade.php
@@ -37,33 +37,29 @@
     </div>
 @endif
 
-<div class="container">
-        <div class="row">
-    @can('create models')
-
-            <div class="col-sm-3">
-                <a class="btn btn-primary" href="{{ route('post.create') }}" title="Add New Post"> <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                  </svg> </a>
-            </div>
-
-            <div class="col-sm-2">
-                <a class="btn btn-primary" href="{{ route('post.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
-            </div>
-            <div class="col-sm-8">
-                <!-- Search form -->
-                    <div class="search-inline">
-                        {!! Form::open(['route' => 'search', 'method' => 'get', 'class' => 'form-contorl form-control-search']) !!}
-                            {!! Form::text('search', null, ['class'=>'form-control form-control-margin', 'required' , 'placeholder=search']) !!}
-                            {!! Form::button('<i class="fa fa-search"></i>', ['type' => 'submit','class'=>'btn btn-secondary']) !!}
-                        {!! Form::close() !!}
-                    </div>
-            </div>
+    <div style="width: 100%; display:flex;">
+@can('create models')
+        <div class="search-inline">
+            <a class="btn btn-primary" href="{{ route('post.create') }}" title="Add New Post"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+            </a>
         </div>
+@endcan
+
+        <div class="search-inline">
+            <a class="btn btn-primary" href="{{ route('post.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+        </div>
+            <!-- Search form -->
+        <div class="search-inline" style="width: 100%; display:grid">
+            {!! Form::open(['route' => 'search', 'method' => 'get', 'class' => 'form-contorl form-control-search']) !!}
+                {!! Form::text('search', null, ['class'=>'form-control form-control-margin', 'required' , 'placeholder=search']) !!}
+                {!! Form::button('<i class="fa fa-search"></i>', ['type' => 'submit','class'=>'btn btn-secondary']) !!}
+            {!! Form::close() !!}
         </div>
 
-    @endcan
+    </div>
 
 
 
